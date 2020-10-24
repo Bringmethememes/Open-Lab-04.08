38d657270b6ac8a002cdc723aa4ebcdad1a0e57e
PC <PC@ANDREJKOVPOITA>
2020-10-24 14:27:02 +0200

    Listy su OP

--- Open-Lab-04.08/StringTools.cs
+++ Open-Lab-04.08/StringTools.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Open_Lab_04._08
 {
@@ -6,7 +9,22 @@ namespace Open_Lab_04._08
     {
         public string[] IsFourLetters(string[] strings)
         {
-            throw new NotImplementedException();
+            List<string> pleb = new List<string>();
+            for(int i = 0; i < strings.Length;i++)   
+                if (strings[i].Length == 4)
+                {
+                    pleb.Add(strings[i]);
+                }
+                else
+                {
+
+                }
+            string[] bruh = pleb.ToArray();
+            return bruh;
+
+
+
+
         }
     }
 }
